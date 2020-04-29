a7e09125b68e93c3027320adddf4edc7ec02eb0c
Johanna Lömker <jloemker@yahoo.de>
2020-04-29 13:45:06 +0200

    some small corrections

--- macro.c
+++ macro.c
@@ -6,25 +6,24 @@
 #include "TH1.h"
 #include "TH1D.h"
 */
-//Plot the function on top of each the dataset. What do you observe
-
-
-void TreeMacro(){
+void macro(){
 //reading file
 TFile *dataFile = new TFile("data.root");
 //pointer to the histogram 
 TH1D * dataHist =(TH1D*)dataFile -> Get("combined");
 //Draw histogram...Actually E2 should carry the error bars
+dataHist -> Draw("E2");
 
-//But running the program only creates the
-double parameter[4] = {30, -0.06, 800, 0.01};
-
+}
+//Plot the function on top of each the dataset. What do you observe
+/*
 double function(double x, double * parameter){
     double result = parameter[0] + x*parameter[1] + parameter[2]*std::exp(-x*parameter[3]);
     return result;
 }
 
+double parameter[4] = {30, -0.06, 800, 0.01};
+
 TF1 * func = new TF1(func,double function(double x, double * parameter), 3,300);
 func -> Draw();
-dataHist -> Draw("E2");
-}
+*/
