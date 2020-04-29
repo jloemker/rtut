5e6177a2708aa1250d89d36f765f4f50bc2f3082
Johanna Lömker <jloemker@yahoo.de>
2020-04-29 13:45:52 +0200

    empty now -> use for second part

--- TreeMacro.c
+++ TreeMacro.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-/*
-#include "TCanvas.h"
-#include "TFile.h"
-#include "TH1.h"
-#include "TH1D.h"
-*/
-
-void TreeMacro(){
-//reading file
-TFile *dataFile = new TFile("data.root");
-//pointer to the histogram 
-TH1D * dataHist =(TH1D*)dataFile -> Get("combined");
-//Draw histogram...Actually E2 should carry the error bars
-dataHist -> Draw("E2");
-//But running the program only creates the pdf
-}
+//For the second exercise
