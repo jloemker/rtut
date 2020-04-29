57141606c3b3442e0bded1bc4f3c0d26807d1a84
Johanna Lömker <jloemker@yahoo.de>
2020-04-29 15:25:21 +0200

    nothing to include-focus on errorbars

--- macro.c
+++ macro.c
@@ -1,19 +1,11 @@
-#include <stdio.h>
-#include <functional>
-
-#include "TCanvas.h"
-#include "TFile.h"
-#include "TH1.h"
-
-
-
+//Obviously I don't need to include anything
 void macro(){
 
 TH1::AddDirectory(ROOT::kFALSE);//sets global switch: disabling references
 TFile dataFile = TFile("data.root");//reading file
 /*Canvas: area mapped to a window under the control of the display manager
 -Root sessions can have many of them open at any given time*/
-TCanvas * c = new TCanvas("combined", "combined", 400, 500);//1st horiz. 2cond vert.
+TCanvas * c = new TCanvas("combined", "combined", 600, 500);//1st horiz. 2cond vert.
 
 TH1F * dataHist =(TH1F*) dataFile -> Get("combined"); //pointer to the histogram 
 
@@ -22,7 +14,7 @@ func_bg -> SetParameters(30, -0.06, 800, 0.01);
 func_bg -> SetLineColor(kBlue);
 func_bg -> SetLineStyle(kDashDotted);
 
-dataHist -> Draw("E2");//Draw histogram - !Actually E2 should carry the error bars
+dataHist -> Draw("E2");//Draw histogram - !Actually E2 should carry the error bars: Draw error bars with rectangles
 dataHist -> Fit("func_bg");//Draw our func_bg into same Histogram
 }
 //Plot the function on top of each the dataset. What do you observe
