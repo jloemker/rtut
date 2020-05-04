9315692561708334c01b34166f76b5f3ba377091
Johanna Lömker <jloemker@yahoo.de>
2020-05-04 10:32:21 +0200

    new histogram parameter

--- TreeMacro.c
+++ TreeMacro.c
@@ -1,5 +1,6 @@
 void TreeMacro(){
     TH1::AddDirectory(kFALSE);//-> histogram is not gone when the file is closed 
+    gROOT->SetBatch(kTRUE);
 //################################################################################---Open Tfile + TTree for both
  	TFile * bg_File = new TFile("background.root");
     TFile * sig_File = new TFile("signal.root");
@@ -7,8 +8,8 @@ void TreeMacro(){
 	TTree * bg_Tree = (TTree*)bg_File->Get("btree");//pointer to the tree
     TTree * sig_Tree = (TTree*)sig_File->Get("stree");
 //################################################################################---Histograms
-    TH1F * bg_PT = new TH1F("bg_PT","p_T [GeV]",70,0,100);//(name, title, #bins, from, to)
-    TH1F * sig_PT = new TH1F("sig_PT","p_T [GeV]",70,0,100);
+    TH1F * bg_PT = new TH1F("bg_PT","p_T [GeV]",50,0,100);//(name, title, #bins, from, to)
+    TH1F * sig_PT = new TH1F("sig_PT","p_T [GeV]",50,0,100);
 
     TH1F * bg_eta = new TH1F("bg_eta", "pseudo-rapidity", 20, -4.3, 4.3);
     TH1F * sig_eta = new TH1F("sig_eta", "pseudo-rapidity", 20, -7, 7);   
@@ -18,7 +19,7 @@ void TreeMacro(){
 
     TH1F * sig_E = new TH1F("sig_E", "Energy [GeV]", 100, 0, 700);
     TH1F * bg_E = new TH1F("bg_E", "Energy [GeV]", 100, 0, 700);
-//###############################################################################---Selection Histograms -> Canvas missing
+//###############################################################################---Selection Histograms
     TH1F * bg_PT_select= new TH1F("bg_PT_sel","p_T [GeV]",50,0,110);
 	TH1F * sig_PT_select = new TH1F("sig_PT_sel","p_T [GeV]",50,0,110);
 
@@ -28,20 +29,17 @@ void TreeMacro(){
 	TH1F * bg_phi_select = new TH1F("bg_phi_sel","#phi",20,-3.5,3.5);
 	TH1F * sig_phi_select = new TH1F("sig_phi_sel","#phi",20,-3.5,3.5);
 
-	TH1F * bg_E_select = new TH1F("bg_E_sel","Energy [GeV]",100,0,1000);
-	TH1F * sig_E_select = new TH1F("sig_E_sel","Energy [GeV]",100,0,1000);
+	TH1F * bg_E_select = new TH1F("bg_E_sel","Energy [GeV]",100,0,700);
+	TH1F * sig_E_select = new TH1F("sig_E_sel","Energy [GeV]",100,0,700);
+//################################################################################---2D Histograms
+    TH2F * bg_PTvEta = new TH2F("bg_pTvEta","bg_pTvEta",100,0, 110, 20,-7,7);
+	TH2F * sig_PTvEta = new TH2F("sig_pTvEta","sig_pTvEta",100,0,110,20,-7,7);
 
-    TH2F * bg_PTvEta = new TH2F("bg_pTvEta","bg_pTvEta",100,0,110,50,0,1000);
-	TH2F * sig_PTvEta = new TH2F("sig_pTvEta","sig_pTvEta",100,0,110,50,0,1000);
+    TH2F * bg_phivE = new TH2F("bg_phivE","bg_phivE",20,-3.5,3.5,100,0,700);
+	TH2F * sig_phivE = new TH2F("sig_phivE","sig_phivE",20,-3.5,3.5,100,0,700);
 
-    TH2F * bg_pTvEta = new TH2F("bg_pTvEta","bg_pTvEta",100,0,110,50,0,1000);
-    TH2F * sig_pTvEta = new TH2F("sig_pTvEta","sig_pTvEta",100,0,110,50,0,1000);
-
-    TH2F * bg_phivE = new TH2F("bg_phivE","bg_phivE",100,0,110,50,0,1000);
-	TH2F * sig_phivE = new TH2F("sig_phivE","sig_phivE",100,0,110,50,0,1000);
-
-    TH2F * bg_pTvE = new TH2F("bg_pTvE","bg_pTvE",100,0,110,50,0,1000);
-	TH2F * sig_pTvE = new TH2F("sig_pTvE","sig_pTvE",100,0,110,50,0,1000);
+    TH2F * bg_pTvE = new TH2F("bg_pTvE","bg_pTvE",100,0,110,100,0,700);
+	TH2F * sig_pTvE = new TH2F("sig_pTvE","sig_pTvE",100,0,110,100,0,700);
 //################################################################################---Branches and Dataaccess---BG
     TBranch * branchPx = bg_Tree -> GetBranch("px");
     TBranch * branchPy = bg_Tree -> GetBranch("py");
@@ -68,7 +66,7 @@ void TreeMacro(){
 	    bg_eta->Fill(v_bg.Eta());
 	    bg_phi->Fill(v_bg.Phi());
         bg_E->Fill(v_bg.E());
-        bg_pTvEta->Fill(v_bg.Pt(),v_bg.Eta());
+        bg_PTvEta->Fill(v_bg.Pt(),v_bg.Eta());
         bg_phivE->Fill(v_bg.Phi(),v_bg.E());
         bg_pTvE->Fill(v_bg.Pt(),v_bg.E());
         //##############################################################################---selection---BG
@@ -102,7 +100,7 @@ void TreeMacro(){
 	    sig_eta->Fill(v_sig.Eta());
 	    sig_phi->Fill(v_sig.Phi());
         sig_E->Fill(v_sig.E());
-        sig_pTvEta->Fill(v_sig.Pt(),v_sig.Eta());
+        sig_PTvEta->Fill(v_sig.Pt(),v_sig.Eta());
         sig_phivE->Fill(v_sig.Phi(),v_sig.E());
         sig_pTvE->Fill(v_sig.Pt(),v_sig.E());
         //##############################################################################---selection---Sig
@@ -114,7 +112,8 @@ void TreeMacro(){
         }
         } 
 //################################################################################---Canvas 
-    TCanvas * c_PT = new TCanvas("PT","PT",600,600);
+
+    TCanvas * c_PT = new TCanvas("PT","PT",400,400);
     c_PT->SetLogy();// logarithmic view for current pad
     bg_PT->SetLineColor(kRed);
     bg_PT->SetFillColor(kRed);
@@ -124,7 +123,7 @@ void TreeMacro(){
     sig_PT->Draw("HSAME");
     //c_pT->Print("pT.eps");
 
-    TCanvas * c_eta = new TCanvas("eta","Pseudo-Rapidity", 600, 600);
+    TCanvas * c_eta = new TCanvas("eta","Pseudo-Rapidity", 400, 400);
     c_eta->SetLogy();
     bg_eta->SetLineColor(kRed);
     bg_eta->SetFillColor(kRed);
@@ -133,7 +132,7 @@ void TreeMacro(){
     bg_eta->Draw("H");
     sig_eta->Draw("HSAME");
 
-    TCanvas * c_phi = new TCanvas("phi","azimuthal angle", 600, 600);
+    TCanvas * c_phi = new TCanvas("phi","azimuthal angle", 400, 400);
     c_phi->SetLogy();
     bg_phi->SetLineColor(kRed);
     bg_phi->SetFillColor(kRed);
@@ -142,7 +141,7 @@ void TreeMacro(){
     bg_phi->Draw("H");
     sig_phi->Draw("HSAME");
 
-    TCanvas * c_E = new TCanvas("E","Energy in [GeV]", 600, 600);
+    TCanvas * c_E = new TCanvas("E","Energy in [GeV]", 400, 400);
     c_E->SetLogy();
     bg_E->SetLineColor(kRed);
     bg_E->SetFillColor(kRed);
@@ -151,7 +150,7 @@ void TreeMacro(){
     bg_E->Draw("H");
     sig_E->Draw("HSAME");
 //################################################################################---Canvas_selection
-    TCanvas * c_PT_select = new TCanvas("PT_select","PT_select", 500, 500);
+    TCanvas * c_PT_select = new TCanvas("PT_select","PT_select", 400, 400);
     c_PT_select->SetLogy();// logarithmic view for current pad
     bg_PT_select->SetLineColor(kRed);
     bg_PT_select->SetFillColor(kRed);
@@ -161,7 +160,7 @@ void TreeMacro(){
     sig_PT_select->Draw("HSAME");
     //c_pT->Print("pT.eps");
 
-    TCanvas * c_eta_select = new TCanvas("eta_select","Pseudo-Rapidity", 600, 600);
+    TCanvas * c_eta_select = new TCanvas("eta_select","Pseudo-Rapidity", 400, 400);
     c_eta_select->SetLogy();
     bg_eta_select->SetLineColor(kRed);
     bg_eta_select->SetFillColor(kRed);
@@ -170,7 +169,7 @@ void TreeMacro(){
     bg_eta_select->Draw("H");
     sig_eta_select->Draw("HSAME");
 
-    TCanvas * c_phi_select = new TCanvas("phi_select","azimuthal angle", 600, 600);
+    TCanvas * c_phi_select = new TCanvas("phi_select","azimuthal angle", 400, 400);
     c_phi_select->SetLogy();
     bg_phi_select->SetLineColor(kRed);
     bg_phi_select->SetFillColor(kRed);
@@ -179,7 +178,7 @@ void TreeMacro(){
     bg_phi_select->Draw("H");
     sig_phi_select->Draw("HSAME");
 
-    TCanvas * c_E_select = new TCanvas("E_select","Energy in [GeV]", 600, 600);
+    TCanvas * c_E_select = new TCanvas("E_select","Energy in [GeV]", 400, 400);
     c_E_select->SetLogy();
     bg_E_select->SetLineColor(kRed);
     bg_E_select->SetFillColor(kRed);
@@ -188,20 +187,23 @@ void TreeMacro(){
     bg_E_select->Draw("H");
     sig_E_select->Draw("HSAME");
     //##############################################################################---2D
-    TCanvas * c_pTvEta = new TCanvas("pTvEta","pTvEta",600,600);
-	c_pTvEta->SetLogy();
-	bg_pTvEta->Draw("Col");
-	sig_pTvEta->Draw("ColSAME");
+    TCanvas * c_PTvEta = new TCanvas("PTvEta","PTvEta",400,400);
+	c_PTvEta->SetLogy();
+	bg_PTvEta->Draw("Col");
+	sig_PTvEta->Draw("ColSAME");
+    c_PTvEta->SaveAs("PTvEta.pdf");
 
-    TCanvas * c_phivE = new TCanvas("phivE","phivE",600,600);
+    TCanvas * c_phivE = new TCanvas("phivE","phivE",400,400);
 	c_phivE->SetLogy();
 	bg_phivE->Draw("Col");
 	sig_phivE->Draw("ColSAME");
+    c_phivE->SaveAs("phivE.pdf");
 
-    TCanvas * c_pTvE = new TCanvas("pTvE","pTvE",600,600);
+    TCanvas * c_pTvE = new TCanvas("pTvE","pTvE",400,400);
 	c_pTvE->SetLogy();
 	bg_pTvE->Draw("Col");
 	sig_pTvE->Draw("ColSAME");
+    c_pTvE->SaveAs("PTvE.pdf");
     //################################################################################---Prints
     cout << "Signal before Selection : " <<sig_PT->GetEntries() << " Total(with Bg) : " << sig_PT->GetEntries()+bg_PT->GetEntries() << endl;
 	cout  << "SignalEvents/Total: "<<sig_PT->GetEntries()/(sig_PT->GetEntries()+bg_PT->GetEntries()) << endl;
